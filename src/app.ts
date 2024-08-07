2fe3a5cca4ce12b09059396d229138d6fab85a62
Kevin <carterk@gmail.comgit config --global --listgit config --global user.email carterk@gmail.com>
2024-08-07 22:40:47 +0200

    feat: Add medical profile routes for managing user medical profiles

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import authRoutes from './routes/authRoutes';
 import adminRoutes from './routes/adminRoutes';
+import medicalProfileRoutes from './routes/medicalProfileRoutes';
 import { connectDB } from './db';
 import dotenv from 'dotenv';
 
@@ -13,7 +14,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use('/api/auth', authRoutes);
 app.use('/api/admin', adminRoutes);
-
+app.use('/api/medical-profile', medicalProfileRoutes);
 const startServer = async () => {
   await connectDB();
   app.listen(8080, () => {
