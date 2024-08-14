fbd2044ef9a900e97cc9f4826c11c1981e57af2a
vierukundo <vierukundo@gmail.com>
2024-08-14 22:39:35 +0200

    API: complete CRUD for emergency and hotline contacts

--- src/routes/emegergencyContactsRoutes.ts
+++ src/routes/emegergencyContactsRoutes.ts
@@ -3,6 +3,11 @@ import {
   addHotline,
   getUserEmergencyContacts,
   addEmergencyContact,
+  getHotlines,
+  updateHotlineContact,
+  updateUserEmergencyContact,
+  deleteHotlineContact,
+  deleteUserEmergencyContact,
 } from "../controllers/emergencyContactController";
 
 const router = Router();
@@ -10,5 +15,10 @@ const router = Router();
 router.post("/add_emergency_contacts", addEmergencyContact);
 router.post("/add_hotline", addHotline);
 router.get("/get_user_emergency_contacts/:userId", getUserEmergencyContacts);
+router.get("/get_hotlines", getHotlines);
+router.post("/update_emergency_contact", updateUserEmergencyContact);
+router.post("/update_hotline", updateHotlineContact);
+router.post("/delete_emergency_contact/:id", deleteUserEmergencyContact);
+router.post("/delete_hotline/:id", deleteHotlineContact);
 
 export default router;
