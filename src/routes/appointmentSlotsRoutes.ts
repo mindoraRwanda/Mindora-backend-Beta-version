c1010bd25b62f990120414f405d5537bc54473e0
vierukundo <vierukundo@gmail.com>
2024-11-01 21:10:01 +0200

    API: created API to retrieve therapist slots

--- src/routes/appointmentSlotsRoutes.ts
+++ src/routes/appointmentSlotsRoutes.ts
@@ -5,12 +5,17 @@ import {
   getAppointmentAvailableSlotById,
   updateAppointmentAvailableSlot,
   deleteAppointmentAvailableSlot,
+  getTherapistSlots,
 } from "../controllers/appointmentSlotsController";
 
 const router = Router();
 
 router.post("/appointment_available_slots", createAppointmentAvailableSlot);
 router.get("/appointment_available_slots", getAllAppointmentAvailableSlots);
+router.get(
+  "/appointment_available_slots/therapists/:therapistId",
+  getTherapistSlots
+);
 router.get("/appointment_available_slots/:id", getAppointmentAvailableSlotById);
 router.put("/appointment_available_slots/:id", updateAppointmentAvailableSlot);
 router.delete(
