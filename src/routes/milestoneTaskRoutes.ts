1cc1c45e257ff60fdf3cb59c646c70b72bff9846
vierukundo <vierukundo@gmail.com>
2025-03-06 13:42:01 +0200

    Updated swagger doc for treatment milestones

--- src/routes/milestoneTaskRoutes.ts
+++ src/routes/milestoneTaskRoutes.ts
@@ -6,22 +6,43 @@ import {
   updateMilestoneTask,
   deleteMilestoneTask,
 } from "../controllers/milestoneTaskController";
+import { authorize } from "../middleware/rbac.middleware";
 
 const router = express.Router();
 
 // Create a new milestone task
-router.post("/milestone-tasks", createMilestoneTask);
+router.post(
+  "/milestone-tasks",
+  authorize("create_milestone_task"),
+  createMilestoneTask
+);
 
 // Get a single milestone task by ID
-router.get("/milestone-tasks/:id", getMilestoneTaskById);
+router.get(
+  "/milestone-tasks/:id",
+  authorize("get_milestone_task"),
+  getMilestoneTaskById
+);
 
 // Get all milestone tasks
-router.get("/milestone-tasks", getMilestoneTasks);
+router.get(
+  "/milestone-tasks",
+  authorize("get_milestone_tasks"),
+  getMilestoneTasks
+);
 
 // Update a milestone task
-router.put("/milestone-tasks/:id", updateMilestoneTask);
+router.put(
+  "/milestone-tasks/:id",
+  authorize("update_milestone_task"),
+  updateMilestoneTask
+);
 
 // Delete a milestone task
-router.delete("/milestone-tasks/:id", deleteMilestoneTask);
+router.delete(
+  "/milestone-tasks/:id",
+  authorize("delete_milestone_task"),
+  deleteMilestoneTask
+);
 
 export default router;
--- src/routes/symptomLogRoutes.ts
+++ src/routes/symptomLogRoutes.ts
@@ -6,13 +6,30 @@ import {
   updateSymptomLog,
   deleteSymptomLog,
 } from "../controllers/symptomLogsController";
+import { authorize } from "../middleware/rbac.middleware";
 
 const router = Router();
 
-router.post("/symptom-logs", createSymptomLog);
-router.get("/symptom-logs/user/:userId", getSymptomLogs);
-router.get("/symptom-logs/:id", getSymptomLogById);
-router.put("/symptom-logs/:id", updateSymptomLog);
-router.delete("/symptom-logs/:id", deleteSymptomLog);
+router.post("/symptom-logs", authorize("create_symptom_log"), createSymptomLog);
+router.get(
+  "/symptom-logs/user/:userId",
+  authorize("get_user_symptom_logs"),
+  getSymptomLogs
+);
+router.get(
+  "/symptom-logs/:id",
+  authorize("get_symptom_log"),
+  getSymptomLogById
+);
+router.put(
+  "/symptom-logs/:id",
+  authorize("update_symptom_log"),
+  updateSymptomLog
+);
+router.delete(
+  "/symptom-logs/:id",
+  authorize("delete_symptom_log"),
+  deleteSymptomLog
+);
 
 export default router;
